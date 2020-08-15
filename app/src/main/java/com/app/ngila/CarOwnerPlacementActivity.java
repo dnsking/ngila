901b1ad8d9d14dec22cd44d28fbf371eab229303
Niza Siwale <nizaswl@gmail.com>
2020-08-15 18:22:02 +0200

    added car owner activity

---
+++ app/src/main/java/com/app/ngila/CarOwnerPlacementActivity.java
@@ -0,0 +1,14 @@
+package com.app.ngila;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarOwnerPlacementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_owner_placement);
+    }
+}
